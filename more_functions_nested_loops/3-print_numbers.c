4cb33bf940dc295ac0afeecf9a214a5a23397a74
root <root@ip-10-42-185-126.ec2.internal>
2025-09-02 17:17:14 +0000

    fix

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * print_numbers - prints numbers from 0 to 9
  *
@@ -7,9 +6,8 @@
  */
 void print_numbers(void)
 {
-    int n;
-
-    for (n = 0; n <= 9; n++)
-        _putchar(n + '0');
-    _putchar('\n');
+int n;
+for (n = 0; n <= 9; n++)
+_putchar(n + '0');
+_putchar('\n');
 }
