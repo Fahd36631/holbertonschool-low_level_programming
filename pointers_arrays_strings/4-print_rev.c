40b65ee811a495afae1369462bf863dfdf3e766f
root <root@ip-10-42-7-145.ec2.internal>
2025-09-03 16:55:16 +0000

    add new task 4-4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse followed by a new line
+ * @s: input string
+ *
+ * Return: nothing
+ */
+void print_rev(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+
+	while (len > 0)
+	{
+		_putchar(s[len - 1]);
+		len--;
+	}
+	_putchar('\n');
+}
