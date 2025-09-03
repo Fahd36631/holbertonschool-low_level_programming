ea2d90b7904f06570fa9124aff2d415a528a0729
root <root@ip-10-42-105-155.ec2.internal>
2025-09-03 11:43:34 +0000

    add task 5-more_numbers.c

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -2,7 +2,8 @@
 #define MAIN_H
 
 int _putchar(char c);
+void print_most_numbers(void);
 void print_numbers(void);
-void print_most_numbers(void); /* ← أضف هذا */
+void more_numbers(void);
 
 #endif
