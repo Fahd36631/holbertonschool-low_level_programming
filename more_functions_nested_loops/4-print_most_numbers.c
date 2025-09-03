aa7e34d422fd08ba78b5870de39c1df3eadb4766
root <root@ip-10-42-105-155.ec2.internal>
2025-09-03 11:26:06 +0000

    add task 4-print_most_numbers.c

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,18 +1,20 @@
 #include "main.h"
 
 /**
- * print_most_numbers - prints numbers from 0 to 9 except 2 and 4
+ * print_most_numbers - prints numbers from 0 to 9, except 2 and 4
  *
  * Return: nothing
  */
 void print_most_numbers(void)
 {
-	int n;
+    int n;
 
-	for (n = 0; n <= 9; n++)
-{
-if (n != 2 && n != 4)
-_putchar(n + '0');
-}
-_putchar('\n');
+    for (n = 0; n <= 9; n++)
+    {
+        if (n != 2 && n != 4)
+        {
+            _putchar(n + '0');
+        }
+    }
+    _putchar('\n');
 }
