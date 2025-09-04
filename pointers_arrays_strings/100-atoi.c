58d9cb793313150c11fcf79b482d64888a666a51
root <root@ip-10-42-113-117.ec2.internal>
2025-09-04 10:06:24 +0000

    fix

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -8,26 +8,27 @@
  */
 int _atoi(char *s)
 {
-	int i = 0, sign = 1, res = 0, started = 0;
+	int i = 0, sign = 1, num = 0, started = 0;
 
 	while (s[i] != '\0')
 	{
 		if (s[i] == '-')
 			sign *= -1;
 		else if (s[i] == '+')
-			sign *= 1;
+			;
 		else if (s[i] >= '0' && s[i] <= '9')
 		{
 			started = 1;
-			res = res * 10 - (s[i] - '0');
+			if (sign == 1)
+				num = num * 10 + (s[i] - '0');
+			else
+				num = num * 10 - (s[i] - '0');
 		}
 		else if (started)
 			break;
 		i++;
-	{
+	}
 
-	if (sign > 0)
-		res *= -1;
-
-	return (res);
+	return (num);
 }
+
