cb04c608165ce37174a50038647b6773989f6d86
root <root@ip-10-42-111-10.ec2.internal>
2025-09-11 12:10:19 +0000

    fix

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - prints the program name
  * @argc: number of arguments
@@ -8,7 +9,7 @@
  */
 int main(int argc, char *argv[])
 {
-    (void)argc;
-    printf("%s\n", argv[0]);
-    return (0);
+	printf("%s\n", argv[0]);
+	(void)argc;
+	return (0);
 }
