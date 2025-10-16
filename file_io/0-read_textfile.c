27d8890a4fa00952df6845b85624f82391fa5007
root <root@ip-10-42-190-50.ec2.internal>
2025-10-16 07:36:59 +0000

    Fix 0

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,43 +1,49 @@
 #include "main.h"
-#include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
 
 /**
- * read_textfile - reads a text file and prints it to the POSIX standard output
- * @filename: name of the file to read
- * @letters: number of letters to read and print
- *
- * Return: actual number of letters it could read and print, or 0 on failure
+ * read_textfile - read a text file and print to STDOUT
+ * @filename: path to file
+ * @letters: max bytes to read and print
+ * Return: bytes printed, or 0 on error
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	ssize_t bytes_read, bytes_written;
-	char *buffer;
+	ssize_t r;
+	ssize_t w;
+	char *buf;
 
-	if (filename == NULL)
+	if (!filename || !letters)
 		return (0);
 
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
 
-	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
+	buf = malloc(letters);
+	if (!buf)
 	{
 		close(fd);
 		return (0);
 	}
 
-	bytes_read = read(fd, buffer, letters);
-	if (bytes_read == -1)
+	r = read(fd, buf, letters);
+	if (r <= 0)
 	{
-		free(buffer);
+		free(buf);
 		close(fd);
 		return (0);
 	}
 
-	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-	if (bytes_written == -1 ||_
+	w = write(STDOUT_FILENO, buf, r);
+	if (w != r)
+	{
+		free(buf);
+		close(fd);
+		return (0);
+	}
 
+	free(buf);
+	close(fd);
+	return (w);
+}
